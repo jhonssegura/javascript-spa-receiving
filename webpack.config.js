61a24dc272c60ff4a3676f746a98b729482cd9ac
Jhonathan Salas <jhonssegura@outlook.com>
2021-12-23 09:19:40 -0500

    Proyecto finalizado en curso

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebPackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -28,6 +29,10 @@ module.exports = {
                 template: './public/index.html',
                 filename: './index.html',
             }
-        )
+        ),
+        new CopyWebPackPlugin({
+            patterns: [{ from: './src/styles/styles.css',
+            to: '' }],
+        })
     ]
 }
